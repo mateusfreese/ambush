512acb538ae547b30b3cc45cbc64220a997a23bd
x283345 <mateus.santos@southsystem.com.br>
2021-08-08 03:53:36 -0300

    Ktlint format

--- app/src/main/java/com/ambush/ambushchallenge/ui/main/MainFragment.kt
+++ app/src/main/java/com/ambush/ambushchallenge/ui/main/MainFragment.kt
@@ -1,11 +1,12 @@
 package com.ambush.ambushchallenge.ui.main
 
-import androidx.lifecycle.ViewModelProvider
+import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.ambush.ambushchallenge.R
 
 class MainFragment : Fragment() {
@@ -16,15 +17,16 @@ class MainFragment : Fragment() {
 
     private lateinit var viewModel: MainViewModel
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
         return inflater.inflate(R.layout.main_fragment, container, false)
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
     }
-
 }
