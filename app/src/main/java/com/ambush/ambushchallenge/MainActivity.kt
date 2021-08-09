ec951870a8ed8ea5bda85bf40a3ce9102841ca51
x283345 <mateus.santos@southsystem.com.br>
2021-08-09 00:00:04 -0300

    setup base UI and navigation

--- app/src/main/java/com/ambush/ambushchallenge/MainActivity.kt
+++ app/src/main/java/com/ambush/ambushchallenge/MainActivity.kt
@@ -2,17 +2,37 @@ package com.ambush.ambushchallenge
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.ambush.ambushchallenge.ui.main.MainFragment
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.ambush.ambushchallenge.databinding.MainActivityBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: MainActivityBinding
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MainFragment.newInstance())
-                .commitNow()
-        }
+        binding = MainActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupNavigation()
+    }
+
+    private fun setupNavigation() {
+        setSupportActionBar(binding.toolbar)
+
+        navController = findNavController(R.id.navHostFragment)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
